420747c9a2618fb073855ce371f3587c72dd5fb5
josephjaniga <josephjaniga@gmail.com>
2015-06-26 01:49:47 -0400

    setup the projects for github

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class TouchManager : MonoBehaviour {
+
+	[SerializeField]
+	public float rotationSpeedScale = 0.1f;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if ( Input.touches.Length > 0 ){
+			foreach ( Touch touch in Input.touches ){
+
+				if ( touch.phase == TouchPhase.Began ){
+					// began event
+				}
+
+				if ( touch.phase == TouchPhase.Moved ){
+					transform.Rotate(0f, 0f, touch.deltaPosition.y * rotationSpeedScale);
+				}
+
+				TouchPhase[] endPhases = {TouchPhase.Canceled, TouchPhase.Ended};
+				if ( endPhases.Contains(touch.phase) ){
+
+				}
+
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/TouchPainter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchPainter : MonoBehaviour {
+
+	public GameObject p;
+
+	public Transform debug;
+
+	// Use this for initialization
+	void Start () {
+		debug = GameObject.Find("Debug").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if ( Input.touches.Length > 0 ){
+			foreach ( Touch touch in Input.touches ){
+	
+				// create a touch debug
+				if ( touch.phase == TouchPhase.Began ){
+					GameObject temp = Instantiate(p, Vector3.zero, Quaternion.identity) as GameObject;
+					temp.name = "touch"+touch.fingerId;
+					temp.transform.SetParent(debug);
+				}
+
+				// position it
+				if ( touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary ){
+					Transform t = debug.Find("touch"+touch.fingerId);
+					if ( t != null ){
+						Vector3 modifiedScreenPoint = new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane+1f);
+						t.gameObject.transform.position = Camera.main.ScreenToWorldPoint(modifiedScreenPoint);
+					}
+				}
+
+				// destroy it
+				if ( touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled ){
+					Transform t = debug.Find("touch"+touch.fingerId);
+					if ( t != null ){
+						Destroy(t.gameObject);
+					}
+				}
+
+			}
+		}
+
+	}
+}
