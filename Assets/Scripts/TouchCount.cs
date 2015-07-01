9fbd45e038a86c93f4e894387f18574769b93719
josephjaniga <josephjaniga@gmail.com>
2015-07-01 00:13:33 -0400

    resolved issue with physics and touch input

--- Assets/Scripts/TouchCount.cs
+++ Assets/Scripts/TouchCount.cs
@@ -5,9 +5,15 @@ using System.Collections;
 public class TouchCount : MonoBehaviour {
 
 	public GameObject counter;
+	public GameObject phaser;
 	
 	// Update is called once per frame
 	void Update () {
 		counter.GetComponent<Text>().text = Input.touches.Length + "";
+
+		if (Input.touches.Length > 0)
+			phaser.GetComponent<Text>().text = Input.touches[0].phase + "";
+		else 
+			phaser.GetComponent<Text>().text = "Phase";
 	}
 }
--- Assets/Scripts/TouchManager.cs
+++ Assets/Scripts/TouchManager.cs
@@ -4,51 +4,26 @@ using System.Linq;
 
 public class TouchManager : MonoBehaviour {
 
-	[SerializeField]
-	public float rotationSpeedScale = 1f;
-	public Quaternion targetRotation;
+	public bool spinNext;
 
-	public float torqueValue = 0f;
-
-	void Start(){
-		targetRotation = Quaternion.identity;
-	}
+	void Start(){ spinNext = false; }
 
 	void Update(){
-
 		if (Input.touches.Length > 0) {
 			foreach (Touch touch in Input.touches) {
-			
-				Debug.Log (touch.fingerId + " " + touch.phase);
-
-				if (touch.phase == TouchPhase.Began) {}
-				if (touch.phase == TouchPhase.Moved) {}
-				
 				TouchPhase[] endPhases = {TouchPhase.Canceled, TouchPhase.Ended};
 				if (endPhases.Contains (touch.phase)) {
-					if (touch.phase == TouchPhase.Ended) {
-						torqueValue = touch.deltaPosition.y;
-						Debug.Log (torqueValue);
-					}
+					spinNext = true;
 				}
 			}
 		}
-		
-//		if (SystemInfo.deviceType == DeviceType.Desktop ) {
-//			// touch not supported
-//			if ( Input.GetMouseButtonDown(0) ){
-//				torqueValue = 1080f;
-//				Debug.Log (torqueValue);
-//			}
-//		}
-
 	}
 
-	// Update is called once per frame
 	void FixedUpdate () {
-		if (torqueValue > 0f) {
-			transform.GetComponent<Rigidbody> ().AddTorque (new Vector3 (0f, 0f, torqueValue));
-			torqueValue = 0f;
+		if ( spinNext ){
+			transform.GetComponent<Rigidbody> ().AddTorque (new Vector3 (0f, 0f, 1000f));
+			spinNext = false;
 		}
 	}
+
 }
