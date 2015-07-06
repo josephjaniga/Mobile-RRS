09a30583fd0be27c92bb078156c4300b6176c7de
Joe Janiga <josephjaniga@gmail.com>
2015-07-05 23:23:31 -0400

    added level loading, intro screen and web call to image

---
+++ Assets/Scripts/SplashScreenDelayed.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenDelayed : MonoBehaviour {
+
+	public float delayTime = 5f;
+	public bool done = false;
+
+	public string url = "http://i.imgur.com/bUNXwFD.jpg";
+
+	private float timer;
+
+	void Start(){
+		timer = delayTime;
+		StartCoroutine ("Loading");
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void Update(){
+		if (timer > 0) {
+			timer -= Time.deltaTime;
+			return;
+		}
+
+		if (done) {
+			Application.LoadLevel(1);
+		}
+	}
+
+	IEnumerator Loading(){
+
+		// Do something here
+		WWW www = new WWW(url);
+		yield return www;
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = www.texture;
+
+		// Yield until something...
+		//yield return null;
+
+		// If something completed...
+		if (renderer.material.mainTexture != null) {
+			done = true;
+		}
+
+	}
+}
