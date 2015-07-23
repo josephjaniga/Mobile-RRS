4192b82b00a0876150b869e7df97b4c199ac571f
Joe Janiga <josephjaniga@gmail.com>
2015-07-23 00:55:32 -0400

    added the unity mobile adds platform and integrated a test into the level one scene

---
+++ Assets/Scripts/AdvertisingManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class AdvertisingManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Advertisement.Initialize("56858", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Advertisement.IsReady() ){
+			Advertisement.Show();
+		}
+	}
+}
