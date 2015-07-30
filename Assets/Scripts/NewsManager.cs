ca71a4e6391953451334376b7381ef9daad37fa9
Joe Janiga <josephjaniga@gmail.com>
2015-07-29 21:08:44 -0400

    working on the news display objects

---
+++ Assets/Scripts/NewsManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewsManager : MonoBehaviour {
+
+	public GameObject NewsWindow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ToggleNewsWindow(){
+		NewsWindow.SetActive(!NewsWindow.activeSelf);
+	}
+}
